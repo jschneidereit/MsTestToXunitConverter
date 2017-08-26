ac109ef2f992423bc0c3b8cef0fb7587f1b00a90
Jim Schneidereit <jimschneidereit@gmail.com>
2017-08-26 11:39:42 -0700

    Initial solution commit

---
+++ MsTestToXunitConverter/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MsTestToXunitConverter
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+        public void foo()
+        {
+            return;
+        }
+    }
+}
