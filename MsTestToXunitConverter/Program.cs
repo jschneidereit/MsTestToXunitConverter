91d8f4e2e5ab9f888edc79c6d8695dfd12f3a86f
Rikki Gibson <rikkigibson@gmail.com>
2017-08-27 17:19:04 -0700

    MSBuildWorkspace saves rewritten test classes

--- MsTestToXunitConverter/Program.cs
+++ MsTestToXunitConverter/Program.cs
@@ -1,75 +1,20 @@
 ﻿using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp;
-using Microsoft.CodeAnalysis.CSharp.Syntax;
-using Microsoft.CodeAnalysis.MSBuild;
 using System;
-using System.Collections.Generic;
-using System.Collections.Immutable;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace MsTestToXunitConverter
 {
     public class Program
     {
-        static async Task<Compilation> CreateCompilation(string projectFilePath)
-        {
-            var workspace = MSBuildWorkspace.Create();
-            workspace.WorkspaceFailed += Workspace_WorkspaceFailed;
-            var project = await workspace.OpenProjectAsync(projectFilePath);
-            var compilation = await project.GetCompilationAsync();
-            return compilation;
-        }
-
-        private static void Workspace_WorkspaceFailed(object sender, WorkspaceDiagnosticEventArgs e)
-        {
-            Console.WriteLine(e.Diagnostic.Message);
-        }
-
-        static async Task RewriteCompilation(Compilation compilation)
-        {
-            foreach (var st in compilation.SyntaxTrees)
-            {
-                var root = await st.GetRootAsync();
-                if (ShouldRewriteSyntaxTree(root))
-                {
-                    Console.WriteLine("Found a test class: " + st.FilePath);
-                }
-            }
-        }
-
-        static bool ShouldRewriteSyntaxTree(SyntaxNode root)
-        {
-            foreach (var cd in root.DescendantNodes().OfType<ClassDeclarationSyntax>())
-            {
-                foreach (var attr in cd.AttributeLists.SelectMany(al => al.Attributes))
-                {
-                    if (attr.Name is IdentifierNameSyntax id &&
-                        id.Identifier.ValueText == "TestClass")
-                    {
-                        return true;
-                    }
-                }
-            }
-            return false;
-        }
-
-        static async Task ConvertMsTest(string projectFilePath)
-        {
-            var comp = await CreateCompilation(projectFilePath);
-            await RewriteCompilation(comp);
-        }
-
         static int Main(string[] args)
         {
             if (args.Length == 0)
             {
-                Console.WriteLine("Usage: mstestconvert <test-csproj-file>");
+                Console.WriteLine("Usage: mstestconvert <path-to-csproj>");
                 return 1;
             }
 
-            ConvertMsTest(args[0]).Wait();
+            ProjectConverter.ConvertProject(args[0]).Wait();
 
             return 0;
         }
---
+++ MsTestToXunitConverter/ProjectConverter.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.CodeAnalysis.MSBuild;
+using System;
+using System.Threading.Tasks;
+
+namespace MsTestToXunitConverter
+{
+    internal class ProjectConverter
+    {
+        internal static async Task ConvertProject(string projectFilePath)
+        {
+            var workspace = MSBuildWorkspace.Create();
+            var project = await workspace.OpenProjectAsync(projectFilePath);
+            var converter = new Converter();
+            foreach (var docId in project.DocumentIds)
+            {
+                var doc = project.GetDocument(docId);
+                var root = await doc.GetSyntaxRootAsync();
+                var newRoot = converter.Visit(root);
+                if (root != newRoot)
+                {
+                    project = doc
+                        .WithSyntaxRoot(converter.Visit(root))
+                        .Project;
+                }
+            }
+
+            if (!workspace.TryApplyChanges(project.Solution))
+            {
+                Console.WriteLine("Failed to update project at " + projectFilePath);
+            }
+        }
+    }
+}
