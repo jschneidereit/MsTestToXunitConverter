e21f49a1bc1ef6804545733dd1379d15985e0e7b
Jim Schneidereit <jimschneidereit@gmail.com>
2017-09-09 17:58:11 -0700

    Start working on a chocolatey package

---
+++ tools/chocolateyinstall.ps1
@@ -0,0 +1,6 @@
+﻿$ErrorActionPreference = 'Stop'; # stop on all errors
+
+$packageName = $env:ChocolateyPackageName
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+
+Install-BinFile 
